a0dd4ea5429bc41c3cd656bba775b5f98952918c
Pusheeneiro <jankowalski413d@gmail.com>
2017-04-15 17:29:20 +0200

    Menu dzila

---
+++ ColorCheck/app/src/main/java/git_lab/colorcheck/HowTo.java
@@ -0,0 +1,13 @@
+package git_lab.colorcheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HowTo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to);
+    }
+}
--- ColorCheck/app/src/main/java/git_lab/colorcheck/Menu.java
+++ ColorCheck/app/src/main/java/git_lab/colorcheck/Menu.java
@@ -3,14 +3,42 @@ package git_lab.colorcheck;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
+
+    public void openGame(View view){
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+
+    public void openRanking(View view){
+        Intent intent = new Intent(this, Ranking.class);
+        startActivity(intent);
+    }
+
+    public void openTworcy(View view){
+        Intent intent = new Intent(this, Tworcy.class);
+        startActivity(intent);
+    }
+
+    public void openHowTo(View view){
+        Intent intent = new Intent(this, HowTo.class);
+        startActivity(intent);
+    }
+
+
+
 
 }
